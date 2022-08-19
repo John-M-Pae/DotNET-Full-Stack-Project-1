45c8e183cdf884bdf9aff8897a327ce302f777e8
John Pae <johns.regester@gmail.com>
2022-08-19 00:08:50 -0400

    Adds detail to selected items

--- src/src/app/models/flight.ts
+++ src/src/app/models/flight.ts
@@ -5,6 +5,7 @@ export interface Flight {
     arrivalTime: any;
     arrivalAirport: string;
     maxCapacity: number;
+    passengersBooked:number;
 }
 
 export interface FlightDTO {
--- src/src/app/models/passenger.ts
+++ src/src/app/models/passenger.ts
@@ -5,6 +5,7 @@ export interface Passenger {
     job?: string;
     email: string;
     age: number;
+    flightsBooked:number;
 }
 
 export interface PassengerDTO{
